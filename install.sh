1b4ff21fa8947e859d08ec7908fdc0716467b3ec
Tomas Protivinsky <tomas.protivinsky@gmail.com>
2023-11-16 22:40:51 +0100

    modify local bashrc for nvim aliases

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ RED="\e[31m"
 BLUE="\e[34m"
 UNSET="\e[0m"
 
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 export PS1=
 
@@ -29,299 +29,297 @@ set -eo pipefail
 # Depending on the system, we may have curl or wget but not both -- so try to
 # figure it out.
 try_curl() {
-    url=$1; dest=$2; command -v curl > /dev/null && curl -fL $url > $dest
+	url=$1
+	dest=$2
+	command -v curl >/dev/null && curl -fL $url >$dest
 }
 try_wget() {
-    url=$1; dest=$2; command -v wget > /dev/null && wget -O- $url > $dest
+	url=$1
+	dest=$2
+	command -v wget >/dev/null && wget -O- $url >$dest
 }
 
 # Generic download function
 download() {
-    echo "Downloading $1 to $2"
-    [[ -e $(dirname $2) ]] || mkdir -p $(dirname $2)
-    if ! (try_curl $1 $2 || try_wget $1 $2); then
-        echo "Could not download $1"
-    fi
+	echo "Downloading $1 to $2"
+	[[ -e $(dirname $2) ]] || mkdir -p $(dirname $2)
+	if ! (try_curl $1 $2 || try_wget $1 $2); then
+		echo "Could not download $1"
+	fi
 }
-
 
 # Append a line to the end of a file, but only if the line isn't already there
-add_line_to_file () {
-    line=$1
-    file=$2
-    if [ ! -e "$file" ]; then
-        echo "$line" >> $file
-    elif grep -vq "$line" $file; then
-        echo "$line" >> $file
-    fi
+add_line_to_file() {
+	line=$1
+	file=$2
+	if [ ! -e "$file" ]; then
+		echo "$line" >>$file
+	elif grep -vq "$line" $file; then
+		echo "$line" >>$file
+	fi
 }
-
 
 # Prompt user for info ($1 is text to provide)
-ok () {
-    # If the DOTFILES_FORCE=true env var was set, then no need to ask, we want
-    # to always say yes
-    if [[ -v DOTFILES_FORCE && $DOTFILES_FORCE -eq 1 ]]; then
-        return 0
-    fi
-    printf "${GREEN}$1${UNSET}\n"
-    read -p "Continue? (y/[n]) " -n 1 REPLY;
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        return 0
-    fi
-    echo
-    printf "${RED}Exiting.${UNSET}\n"
-    return 1
+ok() {
+	# If the DOTFILES_FORCE=true env var was set, then no need to ask, we want
+	# to always say yes
+	if [[ -v DOTFILES_FORCE && $DOTFILES_FORCE -eq 1 ]]; then
+		return 0
+	fi
+	printf "${GREEN}$1${UNSET}\n"
+	read -p "Continue? (y/[n]) " -n 1 REPLY
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		return 0
+	fi
+	echo
+	printf "${RED}Exiting.${UNSET}\n"
+	return 1
 }
-
 
 clone_if_not_exists() {
-    # make it more robust to transient network issues (seeing many of these...)
-    if [ ! -d "$2" ]; then
-      # Directory doesn't exist. Clone the repo.
-      git clone "$1" "$2"
-    else
-      # Directory exists. Navigate to it and pull the latest changes.
-      CURRENT_DIR=$(pwd)
-      cd "$2" || exit
-      git pull origin master
-      cd $CURRENT_DIR
-    fi 
+	# make it more robust to transient network issues (seeing many of these...)
+	if [ ! -d "$2" ]; then
+		# Directory doesn't exist. Clone the repo.
+		git clone "$1" "$2"
+	else
+		# Directory exists. Navigate to it and pull the latest changes.
+		CURRENT_DIR=$(pwd)
+		cd "$2" || exit
+		git pull origin master
+		cd $CURRENT_DIR
+	fi
 }
 
-
 function copy_dotfiles() {
-    ok "Copies over all the dotfiles here to your home directory.
+	ok "Copies over all the dotfiles here to your home directory.
     - A backup will be made in $BACKUP_DIR
     - List of files that will be copied is in 'include.files'
     - Prompts again before actually running to make sure!"
 
-    files=".bashrc .bash_profile .gitconfig .config/.dircolors .config/git-prompt.sh"
-    for f in $files; do
-        hf=$HOME/$f
-        if [ -r $hf ] && [ ! -h $hf ]; then
-            # file already exists at $HOME and is not a symlink
-            # copy it into .back with timestamp
-            if [ ! -d $HOME/.back ]; then
-                mkdir $HOME/.back
-            fi
-            echo "Moving: $hf -> $HOME/.back"
-            mv $hf $HOME/.back/$f.$(date '+%Y-%m-%d_%H-%M-%S')
-        fi
-        echo "Creating symlink: $hf --> $DOTFILES_DIR/home/$f"
-        ln -sf "$DOTFILES_DIR/home/$f" "$hf"
-    done
-    unset f
-    unset files 
+	files=".bashrc .bash_profile .gitconfig .config/.dircolors .config/git-prompt.sh"
+	for f in $files; do
+		hf=$HOME/$f
+		if [ -r $hf ] && [ ! -h $hf ]; then
+			# file already exists at $HOME and is not a symlink
+			# copy it into .back with timestamp
+			if [ ! -d $HOME/.back ]; then
+				mkdir $HOME/.back
+			fi
+			echo "Moving: $hf -> $HOME/.back"
+			mv $hf $HOME/.back/$f.$(date '+%Y-%m-%d_%H-%M-%S')
+		fi
+		echo "Creating symlink: $hf --> $DOTFILES_DIR/home/$f"
+		ln -sf "$DOTFILES_DIR/home/$f" "$hf"
+	done
+	unset f
+	unset files
 }
 
-
 function install_tmux() {
-    ok "Install tmux and setup its config"
-    sudo apt-get install -y tmux
-    mkdir -p $HOME/.config/tmux
-    ln -sf $DOTFILES_DIR/home/.config/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
-    mkdir -p $HOME/.tmux/plugins
-    clone_if_not_exists https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-    $HOME/.tmux/plugins/tpm/bin/install_plugins
-    
-    # try to fix the catpuccin plugin, to get better names in tabs
-    sed -i "s|local text=\"\$(get_tmux_option \"@catppuccin_window_current_text\" \"#{b:pane_current_path}\")\"|local text=\"\$(get_tmux_option \"@catppuccin_window_current_text\" \"#W [#\(echo '#{pane_current_path}' \| rev \| cut -d'/' -f-2 \| rev\)]\")\"|g" .config/tmux/plugins/tmux/window/window_current_format.sh
-    sed -i "s|local text=\"\$(get_tmux_option \"@catppuccin_window_default_text\" \"#{b:pane_current_path}\")\"|local text=\"\$(get_tmux_option \"@catppuccin_window_default_text\" \"#W [#\(echo '#{pane_current_path}' \| rev \| cut -d'/' -f-2 \| rev\)]\")\"|g" .config/tmux/plugins/tmux/window/window_default_format.sh
-    printf "${YELLOW}patching the catpuccin/tmux plugin win titles${UNSET}\n"
+	ok "Install tmux and setup its config"
+	sudo apt-get install -y tmux
+	mkdir -p $HOME/.config/tmux
+	ln -sf $DOTFILES_DIR/home/.config/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
+	mkdir -p $HOME/.tmux/plugins
+	clone_if_not_exists https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+	$HOME/.tmux/plugins/tpm/bin/install_plugins
 
-    # try to fix vim-tmux-navigator for poetry
-    ln -sf $DOTFILES_DIR/home/.config/tmux/plugins/vim-tmux-navigator/is_vim_fixed.sh $HOME/.config/tmux/plugins/vim-tmux-navigator/is_vim_fixed.sh
-    sed -i '2i\
+	# try to fix the catpuccin plugin, to get better names in tabs
+	sed -i "s|local text=\"\$(get_tmux_option \"@catppuccin_window_current_text\" \"#{b:pane_current_path}\")\"|local text=\"\$(get_tmux_option \"@catppuccin_window_current_text\" \"#W [#\(echo '#{pane_current_path}' \| rev \| cut -d'/' -f-2 \| rev\)]\")\"|g" .config/tmux/plugins/tmux/window/window_current_format.sh
+	sed -i "s|local text=\"\$(get_tmux_option \"@catppuccin_window_default_text\" \"#{b:pane_current_path}\")\"|local text=\"\$(get_tmux_option \"@catppuccin_window_default_text\" \"#W [#\(echo '#{pane_current_path}' \| rev \| cut -d'/' -f-2 \| rev\)]\")\"|g" .config/tmux/plugins/tmux/window/window_default_format.sh
+	printf "${YELLOW}patching the catpuccin/tmux plugin win titles${UNSET}\n"
+
+	# try to fix vim-tmux-navigator for poetry
+	ln -sf $DOTFILES_DIR/home/.config/tmux/plugins/vim-tmux-navigator/is_vim_fixed.sh $HOME/.config/tmux/plugins/vim-tmux-navigator/is_vim_fixed.sh
+	sed -i '2i\
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"\
 "$DIR/is_vim_fixed.sh"\
 is_vim_fixed=$?\
 ' .config/tmux/plugins/vim-tmux-navigator/vim-tmux-navigator.tmux
 
-    sed -i 's/\$is_vim/\$is_vim_fixed/g' .config/tmux/plugins/vim-tmux-navigator/vim-tmux-navigator.tmux
-    printf "${YELLOW}patching the christoomey/vim-tmux-navigator for poetry${UNSET}\n"
+	sed -i 's/\$is_vim/\$is_vim_fixed/g' .config/tmux/plugins/vim-tmux-navigator/vim-tmux-navigator.tmux
+	printf "${YELLOW}patching the christoomey/vim-tmux-navigator for poetry${UNSET}\n"
 }
-
 
 function install_neovim() {
-    ok "Downloads neovim tarball from https://github.com/neovim/neovim, install into $LOCAL_OPT/neovim and create symlink $LOCAL_BIN/nvim"
-    printf "${YELLOW}Installing neovim${UNSET}\n"
-    if [[ $OSTYPE == darwin* ]]; then
-        download https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-macos.tar.gz nvim-macos.tar.gz
-        tar -xzf nvim-macos.tar.gz
-        mv nvim-macos $LOCAL_OPT
-    else
-        download https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux64.tar.gz nvim-linux64.tar.gz
-        tar -xzf nvim-linux64.tar.gz
-        mv nvim-linux64 $LOCAL_OPT/neovim
-        rm nvim-linux64.tar.gz
-    fi
-    ln -sf $LOCAL_OPT/neovim/bin/nvim $LOCAL_BIN/nvim
-    printf "${YELLOW}- installed neovim to $LOCAL_OPT/neovim${UNSET}\n"
-    printf "${YELLOW}- created symlink $LOCAL_BIN/nvim${UNSET}\n"
+	ok "Downloads neovim tarball from https://github.com/neovim/neovim, install into $LOCAL_OPT/neovim and create symlink $LOCAL_BIN/nvim"
+	printf "${YELLOW}Installing neovim${UNSET}\n"
+	if [[ $OSTYPE == darwin* ]]; then
+		download https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-macos.tar.gz nvim-macos.tar.gz
+		tar -xzf nvim-macos.tar.gz
+		mv nvim-macos $LOCAL_OPT
+	else
+		download https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux64.tar.gz nvim-linux64.tar.gz
+		tar -xzf nvim-linux64.tar.gz
+		mv nvim-linux64 $LOCAL_OPT/neovim
+		rm nvim-linux64.tar.gz
+	fi
+	ln -sf $LOCAL_OPT/neovim/bin/nvim $LOCAL_BIN/nvim
+	printf "${YELLOW}- installed neovim to $LOCAL_OPT/neovim${UNSET}\n"
+	printf "${YELLOW}- created symlink $LOCAL_BIN/nvim${UNSET}\n"
 
-    # install kickstart nvim config
-    clone_if_not_exists http://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-    source $HOME/.bashrc
+	# install kickstart nvim config
+	clone_if_not_exists http://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+	source $HOME/.bashrc
 }
-
 
 function install_python() {
-    ok "Installing python3 and python3-venv and create symlink $LOCAL_BIN/python"
-    sudo apt-get install -y python3 python3-venv
-    printf "${YELLOW}- installed python3 and python3-venv ${UNSET}\n"
-    ln -sf $(which python3) $LOCAL_BIN/python
-    printf "${YELLOW}- created symlink $LOCAL_BIN/python${UNSET}\n"
+	ok "Installing python3 and python3-venv and create symlink $LOCAL_BIN/python"
+	sudo apt-get install -y python3 python3-venv
+	printf "${YELLOW}- installed python3 and python3-venv ${UNSET}\n"
+	ln -sf $(which python3) $LOCAL_BIN/python
+	printf "${YELLOW}- created symlink $LOCAL_BIN/python${UNSET}\n"
 }
-
 
 function install_node() {
-    ok "Installing nodejs and npm via nvm"
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    nvm install node
-    printf "${GREEN}nodejs and npm installed.${UNSET}\n"
+	ok "Installing nodejs and npm via nvm"
+	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+	nvm install node
+	printf "${GREEN}nodejs and npm installed.${UNSET}\n"
 }
 
-
 function install_lazygit() {
-    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-    tar xf lazygit.tar.gz lazygit
-    sudo install lazygit $LOCAL_BIN 
-    rm lazygit.tar.gz lazygit
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit $LOCAL_BIN
+	rm lazygit.tar.gz lazygit
 }
 
 function install_lunarvim() {
-    if ! command -v nvim > /dev/null 2>&1; then
-        install_neovim; fi
-    LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+	if ! command -v nvim >/dev/null 2>&1; then
+		install_neovim
+	fi
+	LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 }
-
 
 function install_nvchad() {
-    if ! command -v nvim > /dev/null 2>&1; then
-        install_neovim; fi
-    if [ ! -d $XDG_CONFIG_HOME/nvchad ]; then
-        git clone https://github.com/NvChad/NvChad $XDG_CONFIG_HOME/nvchad --depth 1
-    fi;
+	if ! command -v nvim >/dev/null 2>&1; then
+		install_neovim
+	fi
+	if [ ! -d $XDG_CONFIG_HOME/nvchad ]; then
+		git clone https://github.com/NvChad/NvChad $XDG_CONFIG_HOME/nvchad --depth 1
+	fi
 
-    add_line_to_file "alias nvchad='NVIM_APPNAME=nvchad nvim'" $HOME/.bashrc
+	add_line_to_file "alias nvchad='NVIM_APPNAME=nvchad nvim'" $HOME/.local/.bashrc
 }
-
 
 function install_lazyvim() {
-    if ! command -v nvim > /dev/null 2>&1; then
-        install_neovim; fi
-    if [ ! -d $XDG_CONFIG_HOME/lazyvim ]; then
-        git clone https://github.com/LazyVim/starter $XDG_CONFIG_HOME/lazyvim
-        rm -rf $XDG_CONFIG_HOME/lazyvim/.git
-    fi;
+	if ! command -v nvim >/dev/null 2>&1; then
+		install_neovim
+	fi
+	if [ ! -d $XDG_CONFIG_HOME/lazyvim ]; then
+		git clone https://github.com/LazyVim/starter $XDG_CONFIG_HOME/lazyvim
+		rm -rf $XDG_CONFIG_HOME/lazyvim/.git
+	fi
 
-    add_line_to_file "alias lazyvim='NVIM_APPNAME=lazyvim nvim'" $HOME/.bashrc
+	add_line_to_file "alias lazyvim='NVIM_APPNAME=lazyvim nvim'" $HOME/.local/.bashrc
 }
-
 
 function install_astrovim() {
-    if ! command -v nvim > /dev/null 2>&1; then
-        install_neovim; fi
-    if [ ! -d $XDG_CONFIG_HOME/astrovim ]; then
-        git clone --depth 1 https://github.com/AstroNvim/AstroNvim $XDG_CONFIG_HOME/astrovim
-    fi;
+	if ! command -v nvim >/dev/null 2>&1; then
+		install_neovim
+	fi
+	if [ ! -d $XDG_CONFIG_HOME/astrovim ]; then
+		git clone --depth 1 https://github.com/AstroNvim/AstroNvim $XDG_CONFIG_HOME/astrovim
+	fi
 
-    add_line_to_file "alias astrovim='NVIM_APPNAME=astrovim nvim'" $HOME/.bashrc
+	add_line_to_file "alias astrovim='NVIM_APPNAME=astrovim nvim'" $HOME/.local/.bashrc
 }
-
 
 function install_apt() {
-    ok "Installing additional packages"
-    sudo apt-get install -y build-essential wget curl htop rsync stow
+	ok "Installing additional packages"
+	sudo apt-get install -y build-essential wget curl htop rsync stow
 }
 
-
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -y|--yes) 
-            DOTFILES_FORCE=true
-            shift
-            ;;
-        --dotfiles) 
-            copy_dotfiles
-            shift
-            ;;
-        --tmux) 
-            install_tmux
-            shift
-            ;;
-        --nvim|--neovim)
-            install_neovim
-            shift
-            ;;
-        --python)
-            install_python
-            shift
-            ;;
-        --node)
-            install_node
-            shift
-            ;;
-        --lazygit)
-            install_lazygit
-            shift
-            ;;
-        --lunarvim)
-            install_lunarvim
-            shift
-            ;;
-        --nvchad)
-            install_nvchad
-            shift
-            ;;
-        --lazyvim)
-            install_lazyvim
-            shift
-            ;;
-        --astrovim)
-            install_astrovim
-            shift
-            ;;
-        --nvims)
-            if ! command -v nvim > /dev/null 2>&1; then
-              install_neovim; fi
-            install_lazyvim
-            install_astrovim
-            install_nvchad
-            install_lunarvim
-            shift
-            ;;
-        --apt)
-            install_apt
-            shift
-            ;; 
-        --all)
-            copy_dotfiles
-            if ! command -v tmux > /dev/null 2>&1; then
-              install_tmux; fi
-            if ! command -v nvim > /dev/null 2>&1; then
-              install_neovim; fi
-            if ! command -v node > /dev/null 2>&1; then
-              install_node; fi
-            if ! command -v lazygit > /dev/null 2>&1; then
-              install_lazygit; fi
-            install_python
-            # install_apt
-            exit 1
-            ;;
-        *) 
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
+	case $1 in
+	-y | --yes)
+		DOTFILES_FORCE=true
+		shift
+		;;
+	--dotfiles)
+		copy_dotfiles
+		shift
+		;;
+	--tmux)
+		install_tmux
+		shift
+		;;
+	--nvim | --neovim)
+		install_neovim
+		shift
+		;;
+	--python)
+		install_python
+		shift
+		;;
+	--node)
+		install_node
+		shift
+		;;
+	--lazygit)
+		install_lazygit
+		shift
+		;;
+	--lunarvim)
+		install_lunarvim
+		shift
+		;;
+	--nvchad)
+		install_nvchad
+		shift
+		;;
+	--lazyvim)
+		install_lazyvim
+		shift
+		;;
+	--astrovim)
+		install_astrovim
+		shift
+		;;
+	--nvims)
+		if ! command -v nvim >/dev/null 2>&1; then
+			install_neovim
+		fi
+		install_lazyvim
+		install_astrovim
+		install_nvchad
+		install_lunarvim
+		shift
+		;;
+	--apt)
+		install_apt
+		shift
+		;;
+	--all)
+		copy_dotfiles
+		if ! command -v tmux >/dev/null 2>&1; then
+			install_tmux
+		fi
+		if ! command -v nvim >/dev/null 2>&1; then
+			install_neovim
+		fi
+		if ! command -v node >/dev/null 2>&1; then
+			install_node
+		fi
+		if ! command -v lazygit >/dev/null 2>&1; then
+			install_lazygit
+		fi
+		install_python
+		# install_apt
+		exit 1
+		;;
+	*)
+		echo "Unknown option: $1"
+		exit 1
+		;;
+	esac
 done
 
 printf "${GREEN}Dotfiles installation is complete.${UNSET}\n"
 
 # source $HOME/.bashrc
-
